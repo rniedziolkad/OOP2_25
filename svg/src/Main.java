050f55020ab38c42f921bd9c282edb076c10654a
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2025-02-27 16:19:15 +0100

    translating

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -2,8 +2,16 @@
 public class Main {
     public static void main(String[] args) {
         Point p = new Point();
-        p.x = 5.0;
-        p.y = 3.0;
+        p.x = 20.0;
+        p.y = 20.0;
         System.out.println("P= "+p);
+        System.out.println(p.toSvg());
+
+        p.translate(5, -5);
+        System.out.println("translate: "+p);
+
+        System.out.println("translated: "+p.translated(3, 3));
+        Point translated = p.translated(3, 3);
+        System.out.println("translated 2: "+translated);
     }
 }
--- svg/src/Point.java
+++ svg/src/Point.java
@@ -6,4 +6,21 @@ public class Point {
     public String toString(){
         return "("+x+", "+y+")";
     }
+
+    public String toSvg(){
+        return "<circle r=\"5\" cx=\""+x+"\" cy=\""+y+"\" fill=\"black\" />";
+    }
+
+    public void translate(double dx, double dy){
+        x += dx;
+        this.y += dy;
+    }
+
+    public Point translated(double dx, double dy){
+        Point newPoint = new Point();
+        newPoint.x = x + dx;
+        newPoint.y = this.y + dy;
+
+        return newPoint;
+    }
 }
