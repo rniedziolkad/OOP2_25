5742c65be936433ea7419229a082d87acccd7415
student <student@mail.com>
2025-03-27 15:38:10 +0100

    Dodano Person

--- genealogy/src/Main.java
+++ genealogy/src/Main.java
@@ -1,5 +1,38 @@
+//import java.util.LinkedList;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+//        List<Person> personList = new LinkedList<>();
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person(
+                "Andrzej", "Kowalski",
+                LocalDate.of(1980, 1, 1)
+        ));
+        System.out.println(personList.get(0));
+        personList.add(new Person(
+                "Bartek", "Kowalski",
+                LocalDate.of(2010, 5, 12)
+        ));
+        personList.add(new Person(
+                "Ola", "Kowalska",
+                LocalDate.of(2012, 8, 7)
+        ));
+        System.out.println(personList.get(0).adopt(personList.get(1)));
+        System.out.println(personList.get(0).adopt(personList.get(1)));
+        System.out.println(personList.get(0).adopt(personList.get(0)));
+        System.out.println(personList.get(0).adopt(personList.get(2)));
+
+        System.out.println(personList.size());
+//        personList[1];    -- nie działa
+        for (Person p: personList) {
+            System.out.println(p);
+        }
+        System.out.println();
+        System.out.println(personList.get(0).getYoungestChild());
+        System.out.println(personList.get(1).getYoungestChild());
 
     }
 }
