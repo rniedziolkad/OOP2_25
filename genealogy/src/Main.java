63fd53157d261f0ca55dbd3ff656e49e8119efe9
student <student@mail.com>
2025-04-03 15:52:18 +0200

    wczytywanie i wyjątek NegativeLifespanException

--- genealogy/src/Main.java
+++ genealogy/src/Main.java
@@ -1,7 +1,12 @@
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        List<Person> personList = Person.fromCsv("family.csv");
+        System.out.println(personList.size());
+        for (Person p: personList) {
+            System.out.println(p);
+        }
 
         // Na 4 punkty -- kolokwium I 2024 (3 kroki -- 1 punkt)
         // czas do terminu I kolokwium
