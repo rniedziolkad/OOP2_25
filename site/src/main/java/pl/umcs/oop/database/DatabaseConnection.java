88e2e735a4f6fbedd34273938c5c17a2d897f607
student <student@mail.com>
2025-05-22 16:59:41 +0200

    pakiety

---
+++ site/src/main/java/pl/umcs/oop/database/DatabaseConnection.java
@@ -0,0 +1,29 @@
+package pl.umcs.oop.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void connect(String dbPath) {
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
